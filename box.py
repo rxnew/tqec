b39f3e2421a0b4f16592ff5a32cb2f94b1c89608
Rxnew Axdseuan <rxnew.axdseuan@gmail.com>
2016-09-25 23:01:39 +0900

    commit

--- box.py
+++ box.py
@@ -28,10 +28,33 @@ class Box:
 
         self.type_name = type_name
         self.elements = raw.get('elements', {})
-        self.pure_error_rate = raw.get('error', 0)
+        self.pure_error_rate = raw.get('error', 0.0)
         self.inners = []
 
+        self.check_formats()
+        self.collect_formats()
         self.set_inners()
+
+    def check_formats(self):
+        if not isinstance(self.elements, dict):
+            raise FormatError('Elements is not a dict object.')
+
+        if not isinstance(self.pure_error_rate, float) and \
+           not isinstance(self.pure_error_rate, int):
+            raise FormatError('A pure error rate is not a float or int object.')
+
+    def collect_formats(self):
+        keys = ['bits',
+                'inputs',
+                'outputs',
+                'initializations',
+                'measurements',
+                'operations',
+                'boxes']
+
+        for key in keys:
+            if not key in self.elements:
+                self.elements[key] = []
 
     def set_inners(self):
         pure_success_rate = 1 - self.pure_error_rate
@@ -51,19 +74,19 @@ class Box:
         self.pure_error_rate = 1 - pure_success_rate
 
     def deploy(self, permissible_error_rate, permissible_size):
-        module = Module(self)
-
         inner_modules = []
 
         for (inner, number) in self.inners:
             inner_permissible_error_rate = 0.4
-            inner_permissible_size = (10, 30)
+            inner_permissible_size = [10, 30]
             for i in range(number):
                 inner_module = inner.deploy(inner_permissible_error_rate, inner_permissible_size)
                 inner_modules.append(inner_module)
 
-        module.place(inner_modules)
-        module.parallelize()
-        module.connect()
+        module = Module(self, inner_modules, permissible_error_rate, permissible_size)
 
         return module
+
+class FormatError(Exception):
+    def __init__(self, message):
+        self.message = message
--- module.py
+++ module.py
@@ -1,12 +1,132 @@
-class Module:
-    def __init__(self, box):
-        a = 0
+import re
+import subprocess
 
-    def place(self, inners):
+class ModuleElements:
+    def __init__(self, elements):
+        self.bits            = elements['bits']
+        self.inputs          = elements['inputs']
+        self.outputs         = elements['outputs']
+        self.initializations = elements['initializations']
+        self.measurements    = elements['measurements']
+        self.operations      = elements['operations']
+
+class Module:
+    counter = 0
+    p_gate_type_x = re.compile('braiding|toffoli|mct', re.IGNORECASE)
+
+    @classmethod
+    def get_identity(cls):
+        identity = cls.counter
+        cls.counter += 1
+        return identity
+
+    def __init__(self, box, inners, permissible_error_rate, permissible_size):
+        self.identity        = Module.get_identity()
+        self.type_name       = box.type_name
+        self.size            = [0, 0]
+        self.error_rate      = 0.0
+
+        self.elements        = ModuleElements(box.elements)
+
+        #self.bits            = box.elements['bits']
+        #self.inputs          = box.elements['inputs']
+        #self.outputs         = box.elements['outputs']
+        #self.initializations = box.elements['initializations']
+        #self.measurements    = box.elements['measurements']
+        #self.operations      = box.elements['operations']
+        #self.raw_inners      = []
+
+        self.set_raw_inners(inners)
+
+        self.place(permissible_error_rate, permissible_size)
+        self.parallelize()
+
+    def set_raw_inners(self, inners):
+        for inner in inners:
+            raw_inner = {'type' : inner.type_name,
+                         'size' : inner.size,
+                         'error': inner.error_rate}
+            self.raw_inners.append(raw_inner)
+
+    def place_initializations(self):
+        for initialization in self.elements.initializations:
+            initialization['column'] = 0
+
+    def place_measurements(self):
+        pass
+
+    def place(self, permissible_error_rate, permissible_size):
         a = 0
 
     def parallelize(self):
-        a = 0
+        qo = self.convert_to_qo()
+        file_name = './tmp/' + str(self.identity) + '.qo'
+        f = open(file_name, 'w')
+        for gate in qo:
+            f.write(gate + '\n')
+        f.close()
+
+        cmd = './bin/parallelize ' + file_name
+        #subprocess.check_call(cmd, shell=True, stdout=devnull)
+        #f = open('./tmp/' + str(self.identity) + '_result.qo', 'r')
+        #qo = [gate for gate in f]
+        #f.close()
+        #self.convert_from_qo(qo)
 
     def connect(self):
-        a = 0
+        pass
+
+    def convert_to_qo(self):
+        qo = []
+
+        for operation in self.elements.operations:
+            gate_type = operation.get('type')
+            if Module.p_gate_type_x.match(gate_type):
+                gate_type = 'X'
+
+            bits = operation.get('bits', {})
+            cbits = [self.convert_bit_to_no(bit) for bit in bits.get('controls', [])]
+            tbits = [self.convert_bit_to_no(bit) for bit in bits.get('targets', [])]
+            cbits_str = ' '.join(map(str, cbits))
+            tbits_str = ' '.join(map(lambda no: 'T' + str(no), tbits))
+
+            qo.append(gate_type + ' \\ ' + cbits_str + ' ' + tbits_str + ' \\ \\')
+
+        return qo
+
+    def convert_from_qo(self, qo):
+        operations = []
+
+        for gate in qo:
+            tmp = list(filter(lambda e: e, re.split(r'\s|\\', gate)))
+            gate_type = tmp[0]
+            cbits = list(map(int, filter(lambda e: e[0] != 'T', tmp[1:-1])))
+            tbits = list(map(lambda e: int(e[1:]), filter(lambda e: e[0] == 'T', tmp[1:-1])))
+            step = int(tmp[-1])
+
+            if gate_type == 'X':
+                if len(cbits) == 1:
+                    gate_type = 'braiding'
+                elif len(cbits) == 2:
+                    gate_type = 'toffoli'
+                else:
+                    gate_type = 'mct'
+
+            operation = {'bits': {}}
+            operation['type'] = gate_type
+            operation['bits']['controls'] = list(map(self.convert_no_to_bit, cbits))
+            operation['bits']['targets']  = list(map(self.convert_no_to_bit, tbits))
+            operation['column'] = step * 2 - 1
+
+            operations.append(operation)
+
+        self.elements.operations = operations
+
+    def convert_bit_to_no(self, bit):
+        if not hasattr(self, 'bit_map'):
+            self.bit_map = {bit: i for i, bit in enumerate(self.elements.bits)}
+
+        return self.bit_map.get(bit)
+
+    def convert_no_to_bit(self, no):
+        return self.elements.bits[no]
