f2736f81cfdc8a4879f6c46e03ccf599fbb61174
Rxnew Axdseuan <rxnew.axdseuan@gmail.com>
2016-09-26 15:22:56 +0900

    ディレクトリ変更

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 
+import sys,os
+sys.path.append(os.path.dirname(os.path.abspath(__file__)) + '/lib')
+
 from box import Box
 from module import Module
 
 def main():
-    Module.dump_directory_path = './modules/test/'
+    Module.dump_directory_path = './results/test/'
 
     box = Box('T')
     box.deploy(0.4, (20, 49))
