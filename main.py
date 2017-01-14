e0752d55f7e4bec19abc5559d23e349aa9f96fd3
Rxnew Axdseuan <rxnew.axdseuan@gmail.com>
2017-01-14 21:24:16 +0900

    バグ修正

--- main.py
+++ main.py
@@ -9,12 +9,12 @@ from module import Module
 result_dir = './results'
 
 def main(argv):
-    type_name = argv[1] if len(argv) > 1 else 'T'
+    type_name = argv[1] if len(argv) > 1 else 't'
 
     if not os.path.isdir(result_dir):
         os.makedirs(result_dir)
 
-    Module.dump_directory_path = result_dir + type_name.lower() + '/'
+    Module.dump_directory_path = result_dir + '/' + type_name.lower() + '/'
 
     template = Template(type_name)
     template.deploy(0.001, (30, 49))
